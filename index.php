09a73df6dab71d802cd633177dd6082c8efe33d3
Daniel Gading <dgading@gmail.com>
2015-05-27 10:39:18 -0400

    Adds basic functionality and layout to the site

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title>Daniel Gading</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
@@ -17,8 +17,136 @@
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
+      
+      <div id="wrap">
+      
+      <header class="flexcontainer">
+        <h1 class="site-title">Daniel Gading</h1>
+        <nav class="horiz-nav">
+          <ul>
+            <li><a href="#test">My Work</a></li>
+            <li><a href="#bob">About Me</a></li>
+            <li><a href="#frank">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+      <aside class="welcome-message">
+        Hi, I'm Daniel!<br>
+        Welcome to my little corner of the web.
+      </aside>
+      <section class="container work" id="test">
+        <h1 class="section-title">Some of My Work</h1>
+        <ul class="recent-work">
+					<li class="web">
+            <a href="#">
+					    <figure>
+					    	<img src="http://fillmurray.com/230/300" alt="">
+					    	<figcaption>
+					    		 <h2>Example Work 1</h2>
+					    	</figcaption>
+					    </figure>
+            </a>
+					</li>
+          <li class="web">
+              <a href="#">
+						    <figure>
+						    	<img src="http://fillmurray.com/230/300" alt="">
+						    	<figcaption>
+						    		 <h2>Example Work 2</h2>
+						    	</figcaption>
+						    </figure>
+              </a>
+						</li>
+          <li class="infographic">
+              <a href="#">
+						    <figure>
+						    	<img src="http://fillmurray.com/230/300" alt="">
+						    	<figcaption>
+						    		 <h2>Example Work 3</h2>
+						    	</figcaption>
+						    </figure>
+              </a>
+						</li>
+          <li class="design">
+              <a href="#">
+						    <figure>
+						    	<img src="http://fillmurray.com/230/300" alt="">
+						    	<figcaption>
+						    		 <h2>Example Work 4</h2>
+						    	</figcaption>
+						    </figure>
+              </a>
+						</li>
+          <li class="infographic">
+              <a href="#">
+						    <figure>
+						    	<img src="http://fillmurray.com/230/300" alt="">
+						    	<figcaption>
+						    		 <h2>Example Work 5</h2>
+						    	</figcaption>
+						    </figure>
+              </a>
+						</li>
+          <li class="design">
+              <a href="#">
+						    <figure>
+						    	<img src="http://fillmurray.com/230/300" alt="">
+						    	<figcaption>
+						    		 <h2>Example Work 6</h2>
+						    	</figcaption>
+						    </figure>
+              </a>
+						</li>
+					</ul>
+      </section>
+      <section class="container about" id="bob">
+        <h1 class="section-title">About Me</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis est, ultricies vel molestie vel, molestie at elit. Sed nisi urna, mattis vel ante non, scelerisque mollis dui. Sed id ligula quis neque varius vulputate nec quis turpis. Duis dictum, risus eu cursus lobortis, elit tortor faucibus nulla, eget dignissim lorem augue vitae sem. Fusce nunc dolor, suscipit eget suscipit non, luctus eu leo. Sed scelerisque feugiat massa, ut efficitur orci tincidunt vitae. Phasellus a odio sollicitudin, dignissim justo vel, venenatis quam. Nullam posuere aliquam enim nec sollicitudin. Vivamus consectetur erat eget orci tempus faucibus. Etiam suscipit tincidunt eros. Aliquam auctor, lectus nec suscipit facilisis, magna felis accumsan elit, et iaculis dui enim vestibulum lorem. Nulla mollis sollicitudin euismod. Morbi risus mauris, imperdiet sed libero ornare, rutrum pellentesque est.
 
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+Mauris eu neque at velit pellentesque cursus non sit amet urna. Ut sollicitudin vel metus ac porttitor. Phasellus sit amet euismod magna, ut luctus mi. Donec aliquet tristique eros ullamcorper lacinia. Fusce et purus sed enim venenatis vestibulum eu sit amet lorem. Mauris interdum elementum risus porta varius. Vivamus efficitur augue eros, eget aliquam ante vestibulum at. Donec finibus neque sed ex maximus, a semper ex consequat. Donec et commodo odio. Phasellus mollis in metus et euismod. Nulla sit amet est sagittis, tincidunt turpis id, rhoncus neque. Sed orci leo, bibendum at suscipit quis, interdum at neque. Suspendisse tristique libero sit amet dui pharetra scelerisque. Sed tempus dapibus nunc eget luctus. Donec ut facilisis neque, a consequat erat. Fusce eu arcu sed arcu congue ultricies id eu lorem.
+
+Suspendisse maximus cursus lorem, et molestie lacus hendrerit sed. Sed mattis eros at magna convallis, euismod facilisis metus posuere. Nam feugiat consectetur risus, in malesuada lectus imperdiet non. Duis posuere leo id sem suscipit tincidunt. Donec tincidunt sagittis turpis. Mauris lacus est, posuere quis scelerisque ut, sagittis quis nibh. Nulla ut neque non dolor consectetur lacinia eu in erat. Integer commodo interdum odio. Etiam et malesuada felis, id posuere diam. Sed vel scelerisque dui, auctor dictum metus. Cras cursus, est non volutpat ultricies, orci diam laoreet dolor, eget fringilla sem magna non risus. Suspendisse potenti. Sed eu nulla nunc. Nullam eget arcu nunc. Etiam dapibus ex eu lobortis blandit.
+        </p>
+        <img src="http://fillmurray.com/400/400" alt="">
+      </section>
+      <section class="container contact" id="frank">
+        <h1 class="section-title">Get in Touch</h1>
+        <form class="contact-me">
+          <h2>Contact me directly</h2>
+          <ul>
+            <li>
+              <input id="contact_name" type="text" placeholder="Spiderman" />
+              <label for="contact_name">Your Name</label>
+            </li>
+            <li>
+              <input id="contact_phone" type="email" placeholder="peter@dailybugle.com"/>
+              <label for="contact_phone">Your Email</label>
+            </li>
+            <li>
+              <textarea id="contact_message" placeholder="Can I be an avenger?"></textarea>
+              <label for="contact_message">Your Message</label>
+            </li>
+            <li>
+              <input id="submit" type="submit" value="Submit" />
+            </li>
+          </ul>
+        </form>
+        <nav class="social">
+          <h2>Alternatively you can stalk me on these sites</h2>
+          <ul>
+            <li><a id="linkedin" href="http://www.linkedin.com">LinkedIn</a></li>
+            <li><a id="wordpress" href="http://www.illustratingtheweb.com">My Blog</a></li>
+            <li><a id="github" href="http://www.github.com">Github</a></li>
+          </ul>
+        </nav>
+        
+      </section>
+      <footer>
+        &copy; 2015 Daniel Gading
+      </footer>
+      
+      </div>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
--- js/plugins.js
+++ js/plugins.js
@@ -22,3 +22,75 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+// Sticks the header to the top of the page
+//$(window).scroll(function(eventObject) {
+//  // do stuff! You can find out about how far the window has scrolled via the eventObject.
+//  $el = $('header');
+//    if ($(this).scrollTop() > 300 && $el.css('position') != 'fixed') {
+//        $('header').css({ 'position': 'fixed', 'top': '0', 'left':'0', 'right':'0', 'z-index':'999', 'height':'10vh'});
+//		$('header + .container').css({ 'margin-top': '78px'});
+//    }
+//    if ($(this).scrollTop() < 300 && $el.css('position') == 'fixed') {
+//        $('header').css({ 'position': 'relative', 'top': '', 'left':'', 'right':'', 'height':'90vh' });
+//		$('header + .container ').css({ 'margin-top': '0'});
+//    }
+//});
+
+var wrap = $("#wrap");
+var initHeight = ($(window).height() * 0.75)
+	$("#wrap").on("scroll", function(e) {
+    
+  if (this.scrollTop > initHeight) {
+    wrap.addClass("fix-header");
+    
+  } else {
+    wrap.removeClass("fix-header");
+  }
+  
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+	   $('.container').removeClass('focus-section');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      console.log($('[name=' + this.hash.slice(1) +']'));
+      if (target.length) {
+        $("body, #wrap").animate({
+          scrollTop: target.offset().top - 60 
+        }, 1000);
+		target.addClass('focus-section');
+        console.log(target.offset().top - 60);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+$('#submit').on('mousedown', function(e){
+  e.preventDefault();
+  var $this = $(this),
+      offset = $this.offset(),
+      offsetY = (e.pageY - offset.top),
+      offsetX = (e.pageX - offset.left);
+
+  $this.addClass('clicked').append(
+    $('<span class="btn-circle"></span>')
+      .css({
+        'top' : offsetY,
+        'left' : offsetX
+      })
+  );
+}).on('mouseup mouseout', function(e){
+  e.preventDefault();
+  var $this = $(this);
+  
+  $this.removeClass('clicked')
+    .children().fadeOut(function(){
+      $(this).remove();
+    });
+});
